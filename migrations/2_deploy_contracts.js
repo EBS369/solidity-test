6f404cb66526cea686283113d1ddabec2341956a
Wong Liang Xing <liangxingwong@ieee.org>
2022-02-07 06:03:53 +0800

    rewards in lp not token

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 const StakingRewardsFactory = artifacts.require("StakingRewardsFactory")
 module.exports = function (deployer) {
-    REWARDS_TOKEN = "0xC644559c6A788F4208331d82d7eb42c50c099aA7";
+    REWARDS_TOKEN = "0xd11ee576a8c4c5a27cc833cae4fb5030f27f3673";
     REWARDS_GENESIS = 1644192000; // 2-7 8 HKT
     deployer.deploy(StakingRewardsFactory, REWARDS_TOKEN, REWARDS_GENESIS);
 }
