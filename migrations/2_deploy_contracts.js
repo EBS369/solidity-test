5d729c4b42511615fe739fb0e088c6f51690d438
Wong Liang Xing <liangxingwong@ieee.org>
2022-02-07 05:05:23 +0800

    genesis

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 const StakingRewardsFactory = artifacts.require("StakingRewardsFactory")
 module.exports = function (deployer) {
     REWARDS_TOKEN = "0xC644559c6A788F4208331d82d7eb42c50c099aA7";
-    REWARDS_GENESIS = 1644179708;
+    REWARDS_GENESIS = 1644183505;
     deployer.deploy(StakingRewardsFactory, REWARDS_TOKEN, REWARDS_GENESIS);
 }
