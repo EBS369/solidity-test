9766445e66e8f474c14b4add8ae835b4f1298e6e
Wong Liang Xing <liangxingwong@ieee.org>
2022-02-08 06:54:06 +0800

    reordering

--- contracts/IStakingRewards.sol
+++ contracts/IStakingRewards.sol
@@ -3,17 +3,17 @@ pragma solidity ^0.8.0;
 
 interface IStakingRewards {
     // Views
+    function balanceOf(address account) external view returns (uint256);
+
+    function earned(address account) external view returns (uint256);
+
+    function getRewardForDuration() external view returns (uint256);
+
     function lastTimeRewardApplicable() external view returns (uint256);
 
     function rewardPerToken() external view returns (uint256);
 
-    function earned(address account) external view returns (uint256);
-
     function totalSupply() external view returns (uint256);
-
-    function balanceOf(address account) external view returns (uint256);
-
-    function getRewardForDuration() external view returns (uint256);
 
     function viewLockingTimeStamp(address account)
         external
@@ -21,14 +21,14 @@ interface IStakingRewards {
         returns (uint256);
 
     // Mutative
+    function getReward() external;
+
+    function quit() external;
+
     function stake(uint256 amount) external;
 
     function stakeTransferWithBalance(uint256 amount, address useraddress)
         external;
 
     function withdraw(uint256 amount) external;
-
-    function getReward() external;
-
-    function quit() external;
 }
