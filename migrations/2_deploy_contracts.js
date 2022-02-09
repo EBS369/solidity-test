cd93beb308186f34205bbf199371a59a47c3c8ea
Wong Liang Xing <liangxingwong@ieee.org>
2022-02-09 12:28:08 +0800

    comment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 const StakingRewardsFactory = artifacts.require("StakingRewardsFactory")
 module.exports = function (deployer) {
-    REWARDS_TOKEN = "0xd11ee576a8c4c5a27cc833cae4fb5030f27f3673"; // LP Rewards
+    REWARDS_TOKEN = "0xd11ee576a8c4c5a27cc833cae4fb5030f27f3673"; // $AVAX-$NEW LP
     REWARDS_GENESIS = 1644397200; //  2022/02/09 (YYYY/MM/DD) Wednesday 17:00 HKT
     deployer.deploy(StakingRewardsFactory, REWARDS_TOKEN, REWARDS_GENESIS);
 }
