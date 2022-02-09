86b40648c5b31e126bbae5d78e00e8398cf7ecd0
Wong Liang Xing <liangxingwong@ieee.org>
2022-02-09 11:56:03 +0800

    move interface to folder

--- contracts/StakingRewards.sol
+++ contracts/StakingRewards.sol
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-import "./IStakingRewards.sol";
+import "./interfaces/IStakingRewards.sol";
 import "./RewardsDistributionRecipient.sol";
 
 contract StakingRewards is
