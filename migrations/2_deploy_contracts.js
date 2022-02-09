2d177ca3de371229324d67b5f7125bc8dfd4e399
Wong Liang Xing <liangxingwong@ieee.org>
2022-02-09 13:02:06 +0800

    log ABI to console

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,12 @@
 const StakingRewardsFactory = artifacts.require("StakingRewardsFactory")
+const fs = require('fs');
 
 module.exports = function (deployer) {
     REWARDS_TOKEN = "0xd11ee576a8c4c5a27cc833cae4fb5030f27f3673"; // $AVAX-$NEW LP
     STAKING_REWARDS_GENESIS = 1644397200; //  2022/02/09 (YYYY/MM/DD) Wednesday 17:00 HKT
 
     deployer.deploy(StakingRewardsFactory, REWARDS_TOKEN, STAKING_REWARDS_GENESIS);
+
+    const contract = JSON.parse(fs.readFileSync('./build/contracts/StakingRewardsFactory.json', 'utf8'));
+    console.log(JSON.stringify(contract.abi));
 }
